e81360d058980d4cf86d36f8be20e7f867c2d412
oleg volkov <er556r@mail.ru>
2022-12-13 12:16:39 +0300

    add some cod in main

--- main.cpp
+++ main.cpp
@@ -1,23 +1,32 @@
+// https://github.com/raketanamarse/super_pc/
+
 #include <iostream>
 #include <cmath>
 #include "L.hpp"
 #include "F.hpp"
 #include "vars.hpp"
-
 #include <ctime> // заголовочный файл с прототипом функции clock()
-
 using namespace std;
 
+// формула навигации 
 
 int main(int, char**) {
-    cout << "Hello, world!\n";
+
     unsigned int start_time =  clock(); // начальное время
+    cout << "start prog\n";
 
+    const int kol_el = (NX+1) * (NY+1) * (NZ+1);
+    int i;
+    double *cube = new double[kol_el];
 
+    for(i=0; i<kol_el; i++){
+        kol_el[i]=i;
+    }
+    
 
+    
 
     unsigned int end_time = clock(); // конечное время
     unsigned int search_time = end_time - start_time; // искомое время
-    cout << "work time programma\n";
-     cout << search_time;
+    cout << "runtime = " << search_time << " ms";
 }
