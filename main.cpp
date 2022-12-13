0971731af510e2525506cc5de3ac81b5e872d4f9
oleg volkov <er556r@mail.ru>
2022-12-13 11:59:04 +0300

    add count time

--- main.cpp
+++ main.cpp
@@ -4,9 +4,20 @@
 #include "F.hpp"
 #include "vars.hpp"
 
+#include <ctime> // заголовочный файл с прототипом функции clock()
+
 using namespace std;
 
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    cout << "Hello, world!\n";
+    unsigned int start_time =  clock(); // начальное время
+
+
+
+
+    unsigned int end_time = clock(); // конечное время
+    unsigned int search_time = end_time - start_time; // искомое время
+    cout << "work time programma\n";
+     cout << search_time;
 }
