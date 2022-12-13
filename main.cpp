ea698b48033da44bf6ae446e3915d7e1ba33f6f0
oleg volkov <er556r@mail.ru>
2022-12-13 15:25:09 +0300

    add count x y z of i

--- main.cpp
+++ main.cpp
@@ -1,32 +1,54 @@
 // https://github.com/raketanamarse/super_pc/
 
+
 #include <iostream>
 #include <cmath>
+#include <fstream> 
 #include "L.hpp"
 #include "F.hpp"
 #include "vars.hpp"
 #include <ctime> // заголовочный файл с прототипом функции clock()
 using namespace std;
 
-// формула навигации 
+#define file_name "out.txt"
+
+
+
+void write_to_file(double *massiv){
+    try{
+        ofstream MyFile(file_name);
+
+        // Write to the file
+        for(int i=0; i<N; i++){
+            MyFile << massiv[i] << ' ' << count_X(i) << ' ' << count_Y(i) << ' ' << count_Z(i) <<"\n"; // U x y z
+        }
+        
+        // Close the file
+        MyFile.close();
+    }
+    catch(int cod_error){cout << "error write to " << file_name << "with error code" << cod_error;}
+}
+
+
 
 int main(int, char**) {
 
     unsigned int start_time =  clock(); // начальное время
     cout << "start prog\n";
 
-    const int kol_el = (NX+1) * (NY+1) * (NZ+1);
     int i;
-    double *cube = new double[kol_el];
+    double *cube = new double[N];
 
-    for(i=0; i<kol_el; i++){
-        kol_el[i]=i;
+    for(i=0; i<N; i++){
+        cube[i]=i;
     }
     
 
-    
+    write_to_file(cube);
 
     unsigned int end_time = clock(); // конечное время
     unsigned int search_time = end_time - start_time; // искомое время
     cout << "runtime = " << search_time << " ms";
 }
+
+
--- vars.hpp
+++ vars.hpp
@@ -2,12 +2,27 @@
 
 //Глобальные константы
 
-const int NX = 20, NY = 20, NZ = 20; //Значение количества интервалов по осям X, Y, Z
+const int NX = 21, NY = 21, NZ = 21; //Значение количества точек по осям X, Y, Z
 
 const double HX = 1 / NX, HY = 1 / NY, HZ = 1 / NZ; //Значение шага по осям X, Y, Z
 
 const double DX = 0.25, DY = 0.15, DZ = 0.1; //Значение dx, dy, dz в тензоре D
 
-const double PI = 4 * atan(1); //число Пи
+const double PI = 3.14159265359; //число Пи
 
-const double DT = 0.1; //Значение шага по t
+const double HT = 0.1; //Значение шага по t
+
+const int N = NX * NY * NZ; // общее количество элеметов в массиве (кубе)
+
+int count_zz(int i){
+    return (int) i/(NY*NX);
+}
+
+int count_Y(int i){
+    return (int) (i - count_Z(i)*NY*NX)/NX;
+}
+
+int count_X(int i){
+    return (int) (i - count_Z(i)*NY*NX-count_Y(i)*NX);
+}
+
